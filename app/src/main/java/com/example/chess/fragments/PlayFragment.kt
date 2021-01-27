563682cc1d3745b968427204ede08ecebdf225c6
NikkaOk <nika.okropiridze.1@btu.edu.ge>
2021-01-27 21:21:13 +0400

    cool chess

---
+++ app/src/main/java/com/example/chess/fragments/PlayFragment.kt
@@ -0,0 +1,8 @@
+package com.example.chess.fragments
+
+import androidx.fragment.R
+import androidx.fragment.app.Fragment
+
+class PlayFragment : Fragment(com.example.chess.R.layout.fragment_play) {
+
+}
---
+++ app/src/main/java/com/example/chess/fragments/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.example.chess.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.chess.R
+
+class ProfileFragment: Fragment(R.layout.fragment_profile) {
+
+}
