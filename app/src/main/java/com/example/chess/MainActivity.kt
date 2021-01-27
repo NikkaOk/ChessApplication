ec1cf14df305f80a63016a811b824f4cb096422b
NikkaOk <nika.okropiridze.1@btu.edu.ge>
2021-01-27 22:09:34 +0400

    cool chess

--- app/src/main/java/com/example/chess/MainActivity.kt
+++ app/src/main/java/com/example/chess/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.chess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView : BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appConfig = AppBarConfiguration(setOf(
+            R.id.navigation_play, R.id.navigation_profile
+        ))
+
+        setupActionBarWithNavController(navController, appConfig)
+        navView.setupWithNavController(navController)
+
+
     }
+
+
 }
